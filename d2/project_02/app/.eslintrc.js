12b0e9eca41bd7cc0b208647b55fc5ea5ba76317
Andrii Chemerys <andriichemerys@gmail.com>
2022-03-20 17:15:19 +0100

    Create react app

--- d2/project_02/app/.eslintrc.js
+++ d2/project_02/app/.eslintrc.js
@@ -3,20 +3,27 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react", "prettier"],
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+      },
+    ],
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
   },
 };

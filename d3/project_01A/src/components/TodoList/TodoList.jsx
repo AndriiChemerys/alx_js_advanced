6fc34eb49404c7684431ca40237e6aef51a52bd3
Andrii Chemerys <andriichemerys@gmail.com>
2022-04-02 10:48:45 +0200

    Adding deleting function

---
+++ d3/project_01A/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./TodoList.module.css";
+
+const TodoList = (props) => {
+  if (!Array.isArray(props.todoList) || props.todoList.length === 0) return null;
+
+  return (
+    <ul className={styles.list}>
+      {props.todoList.map((todo, index) => (
+        <li key={index}>
+          {todo.name}
+          <button onClick={() => props.onRemove(todo.name)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+// walidacja propsow - prop-types
+// https://www.npmjs.com/package/prop-types
+
+// TodoList.propTypes = {
+//   todoList: propTypes.Array,
+// };
+
+export default TodoList;

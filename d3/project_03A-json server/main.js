dc0ad30123337e365946cce9b8cd9eab0861c01f
Andrii Chemerys <andriichemerys@gmail.com>
2022-04-02 15:40:48 +0200

    JSON server

--- d3/project_03A-json server/main.js
+++ d3/project_03A-json server/main.js
@@ -1,1 +1,20 @@
-console.log(cars);
+// console.log(cars);
+
+const list = document.querySelector("#list");
+
+fetch("http://localhost:3003/cars")
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((cars) => {
+    // console.log(cars);
+
+    //tutaj sie powinno zadziac dodanie samochodu do HTML
+    cars.forEach((car) => {
+      list.innerHTML += `<li>${car.Name}</li>`;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

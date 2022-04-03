18b4208520317974dc750a109b8301f776862574
Andrii Chemerys <andriichemerys@gmail.com>
2022-04-03 11:03:26 +0200

    Express JS

--- d4/project_06-express_js/server.js
+++ d4/project_06-express_js/server.js
@@ -1,24 +1,18 @@
 import express from "express";
 import fs from "fs";
 
+const fsp = fs.promises;
+
 const app = express();
 
 app.get("/", (req, res) => {
-  fs.readFile("./data/hello.txt", "utf8", (error, data) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-
-    fs.writeFile("./data/hello2.txt", data, "utf8", (error2, data2) => {
-      if (error2) {
-        console.log(error2);
-        return;
-      }
-      console.log("success!");
-    });
-    console.log(data);
-  });
+  fsp
+    .readFile("./data/hello.txt", "utf8")
+    .then((file) => fsp.writeFile("./data/hello2.txt", file, "utf8"))
+    .then((resultFromPreviousPromise) => {
+      console.log("Success?!");
+    })
+    .catch((error) => console.error(error));
 
   res.send("Hello world!");
 });

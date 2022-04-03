a38a60c3e5d2844885b9384acb427be14050bf03
Andrii Chemerys <andriichemerys@gmail.com>
2022-04-03 12:33:00 +0200

    User ID

--- d4/project_07-express_js-generator/routes/users.js
+++ d4/project_07-express_js-generator/routes/users.js
@@ -1,6 +1,3 @@
-// import express from "express";
-// import fs from "fs";
-
 const express = require("express");
 const fs = require("fs");
 
@@ -9,13 +6,32 @@ const fsp = fs.promises;
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", (req, res) => {
-  // res.send(users);
+router.get("/", (req, res) =>
+  fsp.readFile("data/users.json", "utf-8").then((users) => res.send(users))
+);
 
-  fsp
-    .readFile("data/users.json", "utf8")
-    .then((file) => res.send(file))
-    .catch((error) => console.error(error));
+router.get("/:userId", (req, res) => {
+  // console.log(req.params); - parametry ktore przychodza z requestu
+
+  // Zadanie - napisz obsluge tej funkcji. Funkcja ma zwracac obiekt uzytkownika o ID rownego userId przekazanego w req.params
+
+  fsp.readFile("data/users.json", "utf8").then((users) => {
+    const jsUsers = JSON.parse(users);
+    const currentUser = jsUsers.find((user) => user.id === parseInt(req.params.userId, 10));
+
+    if (!currentUser) {
+      return res.status(404).send({ message: "Not Found" });
+    }
+
+    return res.send(currentUser);
+  });
 });
+
+// router.post('/', (req, res) => {
+//   // fsp.readFile('data/users.json', 'utf-8').then(console.log);
+
+//   // // users maja pochodzic z pliku
+//   res.send([]);
+// });
 
 module.exports = router;

8b70425d9a1a138a1a89cd175f8a6e15d674e467
Andrii Chemerys <andriichemerys@gmail.com>
2022-06-11 11:39:25 +0200

    Fauna basics

---
+++ d10/zadanie12-next-fauna/pages/index.js
@@ -0,0 +1,88 @@
+import cn from 'classnames'
+import useSWR, { mutate } from 'swr'
+import { listGuestbookEntries } from '@/lib/fauna'
+
+import { useState } from 'react'
+import EntryItem from '@/components/EntryItem'
+import EntryForm from '@/components/EntryForm'
+import MainLayout from '@/components/MainLayout'
+
+const ENTRIES_PATH = '/api/entries'
+
+const putEntry = (payload) =>
+  fetch(ENTRIES_PATH, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)))
+
+const useEntriesFlow = ({ initialEntries }) => {
+  const { data: entries } = useSWR(ENTRIES_PATH, {
+    initialData: initialEntries,
+  })
+
+  const onSubmit = async (payload) => {
+    await putEntry(payload)
+    await mutate(ENTRIES_PATH)
+  }
+
+  return {
+    entries,
+    onSubmit,
+  }
+}
+
+const Guestbook = ({ entries }) => {
+  const [finalEntries, setFinalEntries] = useState(entries);
+  console.log(entries)
+  // const { entries, onSubmit } = useEntriesFlow({
+  //   initialEntries,
+  // })
+
+  const onSubmit = async() => {
+    await putEntry(entryData)
+    setFinalEntries(finalEntries.concat(entryData))
+    // setFinalEntries([...finalEntries, entryData])
+  }
+
+  return (
+    <MainLayout>
+      <div
+        className={cn(
+          'border border-blue-200 rounded p-6',
+          'my-4 w-full dark:border-gray-800 bg-blue-50',
+          'dark:bg-blue-opaque'
+        )}
+      >
+        <h5
+          className={cn(
+            'text-lg md:text-xl font-bold',
+            'text-gray-900 dark:text-gray-100'
+          )}
+        >
+          Sign the Guestbook
+        </h5>
+        <p className="my-1 text-gray-800 dark:text-gray-200">
+          Share a message for a future visitor.
+        </p>
+        <EntryForm onSubmit={onSubmit} />
+      </div>
+      <div className="mt-4 space-y-8 px-2">
+        {entries?.map((entry) => (
+          <EntryItem key={entry._id} entry={entry} />
+        ))}
+      </div>
+    </MainLayout>
+  )
+}
+
+export const getStaticProps = async () => ({
+  props: {
+    entries: await listGuestbookEntries(),
+  },
+  revalidate: 1,
+})
+
+export default Guestbook
